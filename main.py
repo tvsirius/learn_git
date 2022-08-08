1008aee779e01e938d4a19860426cde00f14f394
Denis Savitsky <densav@gmail.com>
2022-08-08 20:19:27 +0300

    some 555

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from task1 import House
 
-h1=House('Dom1')
-h2=House('Nord',10)
+h1 = House('Dom1')
+h2 = House('Nord', 10)
 
 print(h1)
 print(h2)
-
 
 h2.hot(True)
 h1.cond(True)
@@ -13,10 +12,13 @@ h1.cond(True)
 print(h1)
 print(h2)
 
-
-
-h3=House('Dom2')
-h4=House('Afrika',60)
+h3 = House('Dom2')
+h4 = House('Afrika', 60)
 
 h4.hot()
 
+h3.cond()
+
+print(h1, h2, h3, h4)
+
+print('hello git')
